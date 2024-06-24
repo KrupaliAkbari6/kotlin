5b2180801a01c8e872133c4bd751b3b492b488c8
KrupaliAkbari6 <krupali060802@gmail.com>
2024-06-24 21:17:15 +0530

    add

---
+++ Basics/src/main/kotlin/variable.kt
@@ -0,0 +1,17 @@
+package org.example
+
+fun main() {
+    var first:Int=20 //mutable object
+    println("Before :  $first ")
+    first=90
+    println("After : $first ")
+
+    var point:Int=10
+    point=9032
+    println("Number is : $point")
+
+    var num:Float=13.4f
+    //num=15.7f
+    println("Float value is : $num")
+
+}
